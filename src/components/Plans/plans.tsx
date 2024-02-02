79835719afbfd19d1a86f70e37a2c30c2d93425d
Rallefar1103 <rasmus@turftank.com>
2024-02-01 19:23:20 -0800

    adds the sliding bar for subscriptions

--- src/components/Plans/plans.tsx
+++ src/components/Plans/plans.tsx
@@ -1,8 +1,47 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import styles from "./plans.module.css";
+import SubscriptionSlider from "./subscriptionSlider";
+
+type SubscriptionPlan = "monthly" | "quarterly";
 
 const Plans = () => {
-  return <main className={styles.plans}></main>;
+  const [activePlan, setActivePlan] = useState<SubscriptionPlan>("monthly"); // default to monthly
+
+  const handlePlanChange = (plan: SubscriptionPlan) => {
+    setActivePlan(plan);
+  };
+  return (
+    <main className={styles.plans}>
+      <div className={styles.intro}>
+        <h3>
+          Select a plan based on your needs and travel frequency. We offer plans
+          ranging from gold, emerald, and diamond
+        </h3>
+      </div>
+      <div className={styles.sliderContainer}>
+        <button
+          type="button"
+          className={`${styles.sliderBtn} ${
+            activePlan === "monthly" ? styles.active : ""
+          }`}
+          onClick={() => handlePlanChange("monthly")}
+        >
+          Monthly
+        </button>
+        <button
+          type="button"
+          className={`${styles.sliderBtn} ${
+            activePlan === "quarterly" ? styles.active : ""
+          }`}
+          onClick={() => handlePlanChange("quarterly")}
+        >
+          Quarterly
+        </button>
+      </div>
+    </main>
+  );
 };
 
 export default Plans;
