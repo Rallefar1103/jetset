73d3737be8c2783f2e852ef952e694f82756ace8
Rallefar1103 <rasmus@turftank.com>
2024-02-01 16:24:21 -0800

    adds the call to action page

--- src/components/CallToAction/callToAction.tsx
+++ src/components/CallToAction/callToAction.tsx
@@ -1,8 +1,29 @@
 import React from "react";
 import styles from "./callToAction.module.css";
+import Image from "next/image";
+import ClassyWoman from "../../assets/jetset/woman-exiting-plane.jpeg";
+import ClassyMan from "../../assets/jetset/private-jet-man.jpeg";
 
 const CallToAction = () => {
-  return <main className={styles.callToAction}></main>;
+  return (
+    <main className={styles.callToAction}>
+      <div className={styles.classyLeft}>
+        <Image
+          src={ClassyWoman}
+          alt="classy-img"
+          className={styles.classyImgWoman}
+        />
+        <Image
+          src={ClassyMan}
+          alt="classy-img"
+          className={styles.classyImgMan}
+        />
+      </div>
+      <div className={styles.classyRight}>
+        <p> Call to action </p>
+      </div>
+    </main>
+  );
 };
 
 export default CallToAction;
